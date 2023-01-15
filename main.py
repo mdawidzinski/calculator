5c85095f1a9058ba92da28b9724a6ddbb480bc5a
Marek <mdaw@poczta.fm>
2023-01-15 21:06:54 +0100

    Divide by zero problem solved and some minor code improvements.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
+from tkinter import messagebox
 
 # creating window
 main = Tk()
-main.resizable(0, 0)  # resizing window disabled
+main.resizable(None, None)  # resizing window disabled
 main.title('Calculator')
 main.iconbitmap('calculator_general_icon.ico')
 
@@ -12,7 +13,7 @@ but_font = ('Cambria', '14', 'bold')  # font for buttons
 # creating entry widget
 entry_init = StringVar()
 entry_init.set('0')
-entry = Entry(main, textvariable=entry_init, width=30, borderwidth=10, font=but_font, justify=RIGHT)  # xadługi text
+entry = Entry(main, textvariable=entry_init, width=30, borderwidth=10, font=but_font, justify=RIGHT)
 entry.grid(row=0, columnspan=4)
 
 proces = [] 
@@ -43,32 +44,38 @@ def math(mark):
         entry.insert(0, '0')
     elif len(proces) == 2:
         proces.append(entry.get())
-        res = eval(''.join(proces))
-        proces[0] = str(res)
-        proces[1] = mark
-        proces[:] = proces[:2]
-        entry.delete(0, END)
-        entry.insert(0, '0')
+        try:
+            res = eval(''.join(proces))
+            proces[0] = str(res)
+            proces[1] = mark
+            proces[:] = proces[:2]
+            entry.delete(0, END)
+            entry.insert(0, '0')
+        except ZeroDivisionError:
+            messagebox.showerror('Division Error', 'Cannot divide by zero')
+            clear()
 
 
 def eq():
     if not proces:
-        res = entry.get()
-        entry.delete(0, END)
-        entry.insert(0, res)
+        pass
     else:
         proces.append(entry.get())
-        res = eval(''.join(proces))
-        entry.delete(0, END)
-        if res == int(res):
-            entry.insert(0, str(int(res)))
-        else:
-            entry.insert(0, res)
-        proces[:] = []
+        try:
+            res = eval(''.join(proces))
+            entry.delete(0, END)
+            if res == int(res):  # cuts additional zeros eg. 1.000 -> 1
+                entry.insert(0, str(int(res)))
+            else:
+                entry.insert(0, res)
+            proces[:] = []
+        except ZeroDivisionError:
+            messagebox.showerror('Division Error', 'Cannot divide by zero')
+            clear()
 
 
 # creating buttons
-button_eq = Button(main, text='=', width=30, font=but_font, fg='#0064C8', activeforeground='red', command=lambda: eq())  # lambda a bez lambdy
+button_eq = Button(main, text='=', width=30, font=but_font, fg='#0064C8', activeforeground='red', command=eq)
 button_eq.grid(row=5, columnspan=4)
 
 button0 = Button(main, text='0', width=6, height=3, font=but_font, command=lambda: click('0'))
@@ -77,7 +84,7 @@ button0.grid(row=4, column=0)
 button_sep = Button(main, text='.', width=6, height=3, font=but_font, command=lambda: click('.'))
 button_sep.grid(row=4, column=1)
 
-button_clear = Button(main, text='Clear', width=6, height=3, font=but_font, command=lambda: clear())
+button_clear = Button(main, text='Clear', width=6, height=3, font=but_font, command=clear)
 button_clear.grid(row=4, column=2)
 
 button1 = Button(main, text='1', width=6, height=3, font=but_font, command=lambda: click('1'))
